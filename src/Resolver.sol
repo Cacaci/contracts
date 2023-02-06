73c66f8ae6f05b8012b78ec6bf31d41c327e5afd
jcdenton.eth <jc@beb.xyz>
2023-02-05 19:43:42 -0800

    feat: instantiate contract before

--- src/Resolver.sol
+++ src/Resolver.sol
@@ -3,15 +3,12 @@ pragma solidity ^0.8.17;
 
 import "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
-address constant _bebContract = 0x0F08FC2A63F4BfcDDfDa5c38e9896220d5468a64;
-
 contract Resolver {
     mapping (uint256 => string) map;
+    IERC721 bebContract = IERC721(0x0F08FC2A63F4BfcDDfDa5c38e9896220d5468a64);
 
     function set(uint256 tokenId, string memory _value) external {
-        IERC721 beb = IERC721(_bebContract);
-
-        if (beb.ownerOf(tokenId) == msg.sender) {
+        if (bebContract.ownerOf(tokenId) == msg.sender) {
             map[tokenId] = _value;
         }
     }
