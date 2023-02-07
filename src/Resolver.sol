4209bfdf979fd69b1c936c32970cd61c0d0cc80f
jcdenton.eth <jc@beb.xyz>
2023-02-06 20:10:03 -0800

    feat: add baseUrl

--- src/Resolver.sol
+++ src/Resolver.sol
@@ -5,6 +5,7 @@ import "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
 contract Resolver {
     mapping (uint256 => string) map;
+    string baseUrl = "https://protocol.beb.xyz/graphql";
     IERC721 bebContract = IERC721(0x0F08FC2A63F4BfcDDfDa5c38e9896220d5468a64);
 
     function set(uint256 tokenId, string memory _value) external {
@@ -14,6 +15,13 @@ contract Resolver {
     }
 
     function get(uint256 tokenId) external view returns (string memory) {
+        if (bytes(map[tokenId]).length == 0) {
+            return baseUrl;
+        }
         return map[tokenId];
+    }
+
+    function setBaseUrl(string memory _baseUrl) external {
+        baseUrl = _baseUrl;
     }
 }
